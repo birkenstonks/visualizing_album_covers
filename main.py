1110fc8011cc7df83fb5ae6042b6f737ba3fbcab
Zach Karas <zach.eiouy@gmail.com>
2024-07-06 16:41:22 -0400

    credentials

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import os
+import requests
+import base64
+
+
+CLIENT_ID_PATH = os.path.expanduser("~/.spotify/client_id")
+CLIENT_SECRET_PATH = os.path.expanduser("~/.spotify/client_secret")
+
+with open(CLIENT_ID_PATH, "r") as f:
+    client_id = f.read()
+
+with open(CLIENT_SECRET_PATH, "r") as f:
+    client_secret = f.read()
+
+print(client_id, client_secret)
+
 # 1. get current song
 
 # 2. look online for album cover
