739855eac287ecdbe47b6fb521a1837d65089243
Zach Karas <zach.eiouy@gmail.com>
2024-07-05 20:51:47 -0400

    scaffolding comments

---
+++ main.py
@@ -0,0 +1,5 @@
+# 1. get current song
+
+# 2. look online for album cover
+
+# 3. transfer that information to webpage that'll display the image
---
+++ webpage.py
@@ -0,0 +1,5 @@
+# localhost webpage for displaying album cover
+
+# need to get the current album cover from other python script
+
+# updating will probably be a bit complicated
